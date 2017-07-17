146c4294daff72abed79cdcf2ac90df75ce986e5
Dome-A <5036175@qq.com>
2017-07-17 21:31:25 +0800

    完成公告数据

---
+++ js/my.js
@@ -0,0 +1,17 @@
+// 参数  接口JS
+//var urlpath = "https://60.29.110.78:8443/jeesite";
+//var prepath = "https://60.29.110.78:8443";
+var urlpath = "https://dj.tjftz.gov.cn:8443/jeesite";
+var prepath = "https://dj.tjftz.gov.cn:8443";
+var pageSize = 5; // 组织生活页  每次获取数据条数
+
+
+// 组织生活 接口
+
+
+// 教育培训 接口
+'/a/edu/eduCoursemanage/app_files'
+
+// 个人中心 接口
+
+
